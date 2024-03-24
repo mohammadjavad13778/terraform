6822ab1d330c1fe29d10faaed4d10fa40843c3ee
Amir Hossein Kaveh <amirk1998@outlook.com>
2024-03-24 14:47:53 +0330

    fetal : Add Route Table and IGW

--- main.tf
+++ main.tf
@@ -76,3 +76,23 @@ resource "aws_subnet" "myapp-subnet-1" {
 }
 
 
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+
+}
+
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+}
